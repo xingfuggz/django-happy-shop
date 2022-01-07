95f97295bbf58cb448b13b8e52b2cf28e58ea055
轻编程 <2539909370@qq.com>
2022-01-07 23:27:16 +0800

    2539909370@qq.com

--- DjangoMall/apps/users/models/models.py
+++ DjangoMall/apps/users/models/models.py
@@ -6,7 +6,11 @@ from django.contrib.auth.models import AbstractUser
 
 class DJMallUser(AbstractUser):
     """ 用户模型 """
-    phone = models.CharField("手机号", max_length=50, blank=True, default='')
+    nickename = models.CharField("昵称", max_length=50, blank=True, default="")
+    signa = models.CharField("个性签名", max_length=50, blank=True, default="")
+    phone = models.CharField("手机号", max_length=50, blank=True, default="")
+    address = models.CharField("默认收货地址", max_length=50, blank=True, default="")
+    desc = models.CharField("个人简介", max_length=200, blank=True, default="")
 
     REQUIRED_FIELDS = ['email']
 
