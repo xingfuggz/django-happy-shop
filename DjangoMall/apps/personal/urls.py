8d12384afed1777e33e0819d9dbd43567856ef46
轻编程 <2539909370@qq.com>
2022-01-20 00:02:56 +0800

    模板添加

--- DjangoMall/apps/personal/urls.py
+++ DjangoMall/apps/personal/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("username/<int:pk>/update/", views.DJMallUserUpdateView.as_view(), name="update_username"),
     path("email/<int:pk>/update/", views.DJMallUserUpdateEmailView.as_view(), name="update_email"),
     path('fav_list/', views.DJMallFavoriteListView.as_view(), name='fav_list'),
-    path("fav_delete/<int:pk>/", views.DJMallFavoriteDeleteView.as_view(), name="fav_delete")
+    path("fav_delete/<int:pk>/", views.DJMallFavoriteDeleteView.as_view(), name="fav_delete"),
+    path("address/list/", views.DJMallAddressListView.as_view(), name="address_list"),
+    path("address/create/", views.DJMallAddressCreateView.as_view(), name="address_create"),
 ]
